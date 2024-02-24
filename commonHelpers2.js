8a30e609eddac199095924309c5041b61f06c907
AnnaOsadchuk <119502034+AnnaOsadchuk@users.noreply.github.com>
2024-02-24 15:27:35 +0000

    Deploying to gh-pages from @ AnnaOsadchuk/goit-js-hw-10@ea25392e354b0d61ca756c54f37aa7af4d4e7d81 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/styles-1eb22c97.js";console.log("snack");
+import"./assets/styles-1eb22c97.js";const r=document.querySelector(".form");r.addEventListener("submit",t);function t(o){o.preventDefault(),o.curentTarget,console.dir(o)}
 //# sourceMappingURL=commonHelpers2.js.map
