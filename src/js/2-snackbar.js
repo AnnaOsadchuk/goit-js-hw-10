ea25392e354b0d61ca756c54f37aa7af4d4e7d81
AnnaOsadchuk <119502034+AnnaOsadchuk@users.noreply.github.com>
2024-02-24 16:26:07 +0100

    build command changes

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,9 @@
-console.log('snack');
+const elForm = document.querySelector('.form');
+
+elForm.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const formEl = event.curentTarget;
+  console.dir(event);
+}
